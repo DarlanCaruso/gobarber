0ec2ba2288489a84b2bd66940bc6b728fc8026b6
Darlan Caruso <devcaruso@gmail.com>
2019-04-28 14:54:39 -0300

    Appointment create form

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,8 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
+const AvaliableController = require('./app/controllers/AvaliableController')
 
 // routes
 routes.get('/file/:file', FileController.show)
@@ -27,6 +29,8 @@ routes.get('/', guestMiddleware, SessionController.create)
 routes.get('/signup', guestMiddleware, UserController.create)
 routes.get('/app/logout', SessionController.destroy)
 routes.get('/app/dashboard', DashboardController.index)
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.get('/app/available/:provider', AvaliableController.index)
 
 routes.post('/signin', SessionController.store)
 routes.post('/signup', upload.single('avatar'), UserController.store)
