6d00c97b51f3a07dd57390c465ae042493a9333f
Darlan Caruso <devcaruso@gmail.com>
2019-04-28 12:50:49 -0300

    Listing providers

--- src/routes.js
+++ src/routes.js
@@ -18,19 +18,17 @@ routes.use((req, res, next) => {
 // controllers
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
 
 // routes
+routes.get('/file/:file', FileController.show)
 routes.get('/', guestMiddleware, SessionController.create)
-routes.post('/signin', SessionController.store)
-
 routes.get('/signup', guestMiddleware, UserController.create)
-routes.post('/signup', upload.single('avatar'), UserController.store)
-
 routes.get('/app/logout', SessionController.destroy)
+routes.get('/app/dashboard', DashboardController.index)
 
-routes.get('/app/dashboard', (req, res) => {
-  console.log(req.session.user)
-  return res.render('dashboard')
-})
+routes.post('/signin', SessionController.store)
+routes.post('/signup', upload.single('avatar'), UserController.store)
 
 module.exports = routes
